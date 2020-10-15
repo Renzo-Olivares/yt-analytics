b1ccece02f3c2c989922eb6cd132dc86426efaa0
jstut001 <46539483+jstut001@users.noreply.github.com>
2020-10-15 14:33:31 -0700

    Update CSV_Reader.java

--- src/server/yt-analytics-server/src/main/java/com/bitnbytes/ytanalyticsserver/database/CSV_Reader.java
+++ src/server/yt-analytics-server/src/main/java/com/bitnbytes/ytanalyticsserver/database/CSV_Reader.java
@@ -1,15 +1,40 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class CSV_Reader{
-    private ArrayList<Pair<String, ArrayList<ArrayList<String>>> data;
-    public CSV_Reader(){
-        data = new ArrayList<Pair<String, ArrayList<String>>>();
-    }
+public class CSV_Reader {
+	// File (data - rows X columns)
+	private ArrayList<ArrayList<String>> data;
+	private static final String CSV_SPLIT_BY = ",";
+	
 
-    public void read(String file){
-        //TODO: open csv file and read it to data
-        Pair<String, ArrayList<ArrayList<String>>> p = new Pair<String, ArrayList<ArrayList<String>>>();
-        ArrayList<ArrayList<String>> csv = new ArrayList<ArrayList<String>>();
+	public CSV_Reader() {
+		data = new ArrayList<ArrayList<String>>();
+	}
+
+	public void read(String file) throws IOException{
+    	System.out.println("Removing old data");
+        data.clear();
+        System.out.println("Old data removed\nReading in new data");
+        String line = "";
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+            while ((line = br.readLine()) != null) {
+                ArrayList<String> parse = new ArrayList<String>();
+                for(String s : line.split(CSV_SPLIT_BY)){
+                	parse.add(s);
+                }
+                data.add(parse);
+                
+            }
+
         
+        System.out.println("New data loaded");
     }
+	public final ArrayList<ArrayList<String>> getData() {
+		return data;
+	}
+	
 }
