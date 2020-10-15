af02a6d9a6edb0a9a927dd406b1a12b4938a8f37
jstut001 <46539483+jstut001@users.noreply.github.com>
2020-10-15 13:22:42 -0700

    Create CSV_Reader.java

---
+++ src/server/yt-analytics-server/src/main/java/com/bitnbytes/ytanalyticsserver/database/CSV_Reader.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CSV_Reader{
+    private ArrayList<Pair<String, ArrayList<ArrayList<String>>> data;
+    public CSV_Reader(){
+        data = new ArrayList<Pair<String, ArrayList<String>>>();
+    }
+
+    public void read(String file){
+        //TODO: open csv file and read it to data
+        Pair<String, ArrayList<ArrayList<String>>> p = new Pair<String, ArrayList<ArrayList<String>>>();
+        ArrayList<ArrayList<String>> csv = new ArrayList<ArrayList<String>>();
+        
+    }
+}
