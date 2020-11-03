d0c0a082baaf2fd9f4195a5e11812fb62867bc5a
jstut001 <46539483+jstut001@users.noreply.github.com>
2020-11-03 08:25:14 -0800

    Added required import

--- src/server/yt-analytics-server/src/main/java/com/bitnbytes/ytanalyticsserver/database/EntityManager.java
+++ src/server/yt-analytics-server/src/main/java/com/bitnbytes/ytanalyticsserver/database/EntityManager.java
@@ -12,6 +12,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Collections;
 
 @Component("entityManager")
 public class EntityManager {
